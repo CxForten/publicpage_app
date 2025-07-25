fb8253809657106562d75eaad1a976b71221622e
CxForten <chrstfrchvrr14@gmail.com>
2025-07-25 17:13:49 -0500

    responsive desing

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -9,10 +9,11 @@ import { AuthService } from './auth.service';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   isLoggedIn$: Observable<boolean>;
+  menuOpen: boolean = false; 
   constructor(private auth: AuthService) {
     this.isLoggedIn$ = this.auth.isLoggedIn$;
   }
