4ce93ab21a670f960a5817f9f2800a00c2c4f343
CxForten <chrstfrchvrr14@gmail.com>
2025-07-29 22:06:09 -0500

    registro final desing

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,65 +1,101 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,  // necesario para HttpClient
+    RouterModule       // necesario para routerLink
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
   registerForm: FormGroup;
-  cedulaValida: boolean = false;
-  mostrarEmpresa: boolean = false;
+  cedulaValida = false;
+  mostrarEmpresa = false;
+  floatElements = Array.from({ length: 20 });
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.registerForm = this.fb.group({
-      cedula: ['', [Validators.required]],
-      nombres: ['', [Validators.required]],
-      apellidos: ['', [Validators.required]],
-      correo: ['', [Validators.required, Validators.email]],
-      contraseña: ['', Validators.required],
-      confirmar: ['', Validators.required],
+      cedula: [
+        '',
+        [Validators.required]
+      ],
+      nombres: [
+        '',
+        [Validators.required]
+      ],
+      apellidos: [
+        '',
+        [Validators.required]
+      ],
+      correo: [
+        '',
+        [Validators.required, Validators.email]
+      ],
+      contraseña: [
+        '',
+        [Validators.required]
+      ],
+      confirmar: [
+        '',
+        [Validators.required]
+      ],
       nombre_empresa: [''],
       ruc: [''],
       correo_empresarial: [''],
       cargo: [''],
-      terminos: [false, Validators.requiredTrue]
+      terminos: [
+        false,
+        [Validators.requiredTrue]
+      ]
     });
   }
 
-  validarCedula() {
+  validarCedula(): void {
     const cedula = this.registerForm.get('cedula')?.value;
-    this.http.get<any>(`http://localhost:3000/api/validate/${cedula}`).subscribe({
-      next: (data) => {
-        this.registerForm.patchValue({
-          nombres: data.nombres,
-          apellidos: data.apellidos
-        });
-        this.cedulaValida = true;
-      },
-      error: () => {
-        alert('Cédula no encontrada');
-        this.cedulaValida = false;
-      }
-    });
+    this.http
+      .get<{ nombres: string; apellidos: string }>(
+        `http://localhost:3000/api/validate/${cedula}`
+      )
+      .subscribe({
+        next: data => {
+          this.registerForm.patchValue({
+            nombres: data.nombres,
+            apellidos: data.apellidos
+          });
+          this.cedulaValida = true;
+        },
+        error: () => {
+          alert('Cédula no encontrada');
+          this.cedulaValida = false;
+        }
+      });
   }
 
-  registrar() {
-    if (this.registerForm.valid) {
-      const datos = this.registerForm.value;
-      this.http.post('http://localhost:3000/api/auth/register', datos).subscribe({
-        next: () => alert('Usuario registrado'),
-        error: (err) => alert('Error al registrar: ' + err.message)
-      });
-    } else {
+  registrar(): void {
+    if (!this.registerForm.valid) {
       alert('Formulario inválido');
+      return;
     }
+
+    this.http
+      .post('http://localhost:3000/api/auth/register', this.registerForm.value)
+      .subscribe({
+        next: () => alert('Usuario registrado'),
+        error: err => alert('Error al registrar: ' + err.message)
+      });
   }
 }
-
